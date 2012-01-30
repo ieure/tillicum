d18ee07c1848e3ac5b027a950237f36926b44b77
Ian Eure <ian.eure@gmail.com>
2012-01-30 08:49:21 -0800

    Bump version.

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 #
-# © 2011 SimpleGeo, Inc All rights reserved.
+# © 2011, SimpleGeo, Inc All rights reserved.
+# © 2012, Ian Eure.
 # Author: Ian Eure <ian@simplegeo.com>
 #
 
 from setuptools import setup, find_packages
 
 setup(name="tillicum",
-      version="0.8.0",
+      version="0.8.1",
       packages=find_packages(),
       test_suite="nose.collector",
       tests_require=['nose',
